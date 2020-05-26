e9dfc0a09e1604389bc6d7695b5fda80385f2099
Naman Anand <iamnamananand996@gmail.com>
2020-05-26 23:31:25 +0530

    Update GraphUtil.py

--- GraphUtil.py
+++ GraphUtil.py
@@ -5,28 +5,18 @@ A script having a set of simple functions to check graph properties.
 
 
 class UndirectedGraph:
-
     def __init__(self, V, E):
-        """
-        Initialize the Graph class. Accepts a set of vertices V and edges E as input.
-        V is implemented as a set
-        E is implemented as an adjacency list in the form of a dictionary.
-        """
-
         self.V = V
         self.E = E
 
     def __str__(self):
-
         graph_str = ''
-
         for start in self.E.keys():
             for stop in self.E[start]:
                 graph_str += start + ' ' + stop + '\n'
-
         return graph_str
-
-
+    
+    
 def check_vertex_cover(G, S):
     """
     Returns True if set S is a vertex cover of the edge set E,
